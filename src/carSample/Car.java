3495c01a40b2717a498f7bc72206c33caa26bb44
SHIMIZU Katsuya <katsuyashimizu@gmail.com>
2019-04-21 15:01:54 +0900

    add the first car sample(Lesson8.4)

---
+++ src/carSample/Car.java
@@ -0,0 +1,11 @@
+package carSample;
+
+public class Car {
+	int num;
+	double gas;
+
+	void show() {
+		System.out.println("車のナンバーは" + num + "です。");
+		System.out.println("ガソリン量は" + gas + "です。");
+	}
+}
---
+++ src/carSample/Sample1.java
@@ -0,0 +1,11 @@
+package carSample;
+
+public class Sample1 {
+	public static void main(String[] args) {
+		Car car1 = new Car();
+		car1.num = 1234;
+		car1.gas = 40.2;
+
+		car1.show();
+	}
+}
