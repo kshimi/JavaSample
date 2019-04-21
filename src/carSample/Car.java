5cc3fd952e95c4ca929fb4f2b2a21032440554c2
SHIMIZU Katsuya <katsuyashimizu@gmail.com>
2019-04-21 16:17:57 +0900

    private access sample(Lesson9.1)

--- src/carSample/Car.java
+++ src/carSample/Car.java
@@ -1,23 +1,51 @@
 package carSample;
 
 public class Car {
-	int num;
-	double gas;
+	private int num;
+	private double tankCapacity;
+	private double gas = 0.0;
 
+	// 距離を指定して車を走らせます
 	double run(double distance) {
-		gas -= (distance * 0.1);
-		System.out.println(distance + "km 走りました。");
+		double consumeGas = distance * 0.1;
+		if (addGas(consumeGas * -1)) {
+			System.out.println(distance + "km 走りました。");
+		}
 
 		return gas;
 	}
 
+	// 給油量を指定して給油します。
 	void refuel(double amount) {
-		gas += amount;
-		System.out.println(amount + "l 給油しました。");
+		if (addGas(amount)) {
+			System.out.println(amount + "l 給油しました。");
+		}
 	}
 
+	// 車のナンバーとタンク容量を設定します。
+	void makeCar(int num, double tankCapacity) {
+		this.num = num;
+		this.tankCapacity = tankCapacity;
+	}
+
+	// 車の状態を表示します。
 	void show() {
 		System.out.println("車のナンバーは" + num + "です。");
+		System.out.println("車のタンク容量は" + tankCapacity + "l です。");
 		System.out.println("ガソリン量は" + gas + "です。");
+	}
+
+	// ガソリンの残量をチェックしてガソリンの量を計算します。
+	private boolean addGas(double amount) {
+		if (amount > 0 && (gas + amount) > tankCapacity) {
+			System.out.println("タンクが溢れます！ 給油を中止します。");
+			return false;
+		} else if (amount < 0 && (gas + amount) < 0.0) {
+			System.out.println("ガス欠になります！ 止まります。");
+			return false;
+		}
+
+		gas += amount;
+		return true;
 	}
 }
--- src/carSample/Sample1.java
+++ src/carSample/Sample1.java
@@ -3,9 +3,8 @@ package carSample;
 public class Sample1 {
 	public static void main(String[] args) {
 		Car car1 = new Car();
-		car1.num = 1234;
-		car1.gas = 20.2;
-
+		car1.makeCar(1234, 42.5);
+		car1.refuel(40.5);
 		car1.show();
 
 		double remainGas = car1.run(130.7);
@@ -14,5 +13,7 @@ public class Sample1 {
 			car1.refuel(20.0);
 		}
 		car1.show();
+
+		car1.run(300);
 	}
 }
